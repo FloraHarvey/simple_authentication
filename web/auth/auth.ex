73d69f24b49a298dd95d9333b621b470464fbe2b
Flora Harvey <fharvey555@gmail.com>
2017-07-03 14:49:17 +0100

    Refactor user auth out of sessions controller to separate module

---
+++ web/auth/auth.ex
@@ -0,0 +1,32 @@
+defmodule SimpleAuth.Auth do
+
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+
+  alias SimpleAuth.User
+  alias SimpleAuth.Repo
+
+  def login(conn, user) do
+    conn
+    |> Guardian.Plug.sign_in(user)
+  end
+
+  def login_with_email_password(conn, email, password) do
+    user = Repo.get_by(User, email: email)
+
+    cond do
+      user && checkpw(password, user.password_hash) ->
+        {:ok, login(conn, user)}
+      user ->
+        {:error, :unauthorized, conn}
+      true ->
+        dummy_checkpw()
+        {:error, :not_found, conn}
+      end
+  end
+
+  def logout(conn) do
+    Guardian.Plug.sign_out(conn)
+  end
+
+
+end
--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,10 +1,6 @@
 defmodule SimpleAuth.SessionController do
   use SimpleAuth.Web, :controller
 
-  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
-
-  alias SimpleAuth.User
-
   plug :scrub_params, "session" when action in ~w(create)a
 
   def new(conn, _) do
@@ -12,38 +8,27 @@ defmodule SimpleAuth.SessionController do
   end
 
   def create(conn, %{"session" => %{"email" => email, "password" => password}}) do
-    user = Repo.get_by(User, email: email)
-    result = cond do
-      user && checkpw(password, user.password_hash) ->
-        {:ok, login(conn, user)}
-      user ->
-        {:error, :unauthorized, conn}
-      true ->
-        dummy_checkpw()
-        {:error, :not_found, conn}
+    case SimpleAuth.Auth.login_with_email_password(conn, email, password) do
+      {:ok, conn} ->
+        conn
+        |> put_flash(:info, "You’re now logged in!")
+        |> redirect(to: page_path(conn, :index))
+      {:error, _reason, conn} ->
+        conn
+        |> put_flash(:error, "Invalid email/password combination")
+        |> render("new.html")
     end
-
-    case result do
-    {:ok, conn} ->
-      conn
-      |> put_flash(:info, "You’re now logged in!")
-      |> redirect(to: page_path(conn, :index))
-    {:error, _reason, conn} ->
-      conn
-      |> put_flash(:error, "Invalid email/password combination")
-      |> render("new.html")
-    end
-
   end
 
   def delete(conn, _) do
-
-  end
-
-  defp login(conn, user) do
     conn
-    |> Guardian.Plug.sign_in(user)
+    |> SimpleAuth.Auth.logout
+    |> put_flash(:info, "See you later!")
+    |> redirect(to: page_path(conn, :index))
   end
+
+
+
 
 
 end
