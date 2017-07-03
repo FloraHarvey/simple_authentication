add39f93c10e645b11c69044ed8e13ccc0f2ecc1
Flora Harvey <fharvey555@gmail.com>
2017-07-03 17:10:18 +0100

    Add user posts routes/controller/view

--- web/router.ex
+++ web/router.ex
@@ -19,6 +19,14 @@ defmodule SimpleAuth.Router do
     plug SimpleAuth.CurrentUser
   end
 
+  pipeline :login_required do
+
+  end
+
+  pipeline :admin_required do
+
+  end
+
   scope "/", SimpleAuth do
     pipe_through [:browser, :with_session] # Use the default browser stack
 
@@ -27,6 +35,23 @@ defmodule SimpleAuth.Router do
     resources "/users", UserController, only: [:show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
 
+
+    scope "/" do
+      pipe_through [:login_required]
+
+      resources "/users", UserController, only: [:show] do
+        resources "/posts", PostController, only: [:index, :show]
+      end
+
+
+      scope "admin", Admin, as: :admin do
+        pipe_through :admin_required
+
+        resources "users", UserController, only: [:index, :show] do
+          resources "/posts", PostController, only: [:index, :show]
+        end
+      end
+    end
   end
 
 end
